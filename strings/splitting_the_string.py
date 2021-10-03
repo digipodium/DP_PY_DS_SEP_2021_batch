5fd5c349bef42ecbe47493e00b0c856621e76315
Zaid Kamil <xaidmetamorphos@gmail.com>
2021-10-03 20:10:47 +0530

    string done

--- strings/splitting_the_string.py
+++ strings/splitting_the_string.py
@@ -16,6 +16,15 @@ print(gibberish)
 print(f"we found {len(words)} words in msg")
 print(f"we found {len(msg2.split())} words in msg2")
 
+msg3 = 'welcome to wakanda, you will have fun, just dont steal from us, or you will have no fun'
+
+print('n ->', msg3.split(','))
+print('n 2 split ->', msg3.split(',', maxsplit=2))
+print('r ->', msg3.rsplit(','))
+print('r 2 split ->', msg3.rsplit(',', maxsplit=2))
+
+print(msg3[:18])
+
 
 
 
